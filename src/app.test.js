79a3e1fa99c851f4e2e1b135b3e9248ed5ac6973
Martin Lofaj <lofajmatko@gmail.com>
2021-04-04 10:49:07 +0200

    fix sample tests

--- src/app.test.js
+++ src/app.test.js
@@ -1,18 +1,26 @@
 const request = require('supertest');
 const app = require('./app');
 const todoService = require('./service/todo-service');
+const validateJwt = require('./infra/jwt-auth');
 
 // mock implementation detail
 jest.mock('./service/todo-service');
+jest.mock('./infra/jwt-auth');
 
 describe('Fetch todo', () => {
   test('It should response the GET method', async () => {
     // given
-    const expectedResponseBody = { description: 'Test Item', id: 11111 };
+    const expectedResponseBody = { description: 'Test Item', id: 11111, status: 'open' };
     todoService.getTodo.mockResolvedValue(expectedResponseBody);
+    validateJwt.mockImplementation((req, res, next) => {
+      req.user = { sub: 'test-user-id' };
+      next();
+    });
 
     // when
-    const response = await request(app).get('/api/v1/todos/1');
+    const response = await request(app)
+      .get('/v1/todos/1')
+      .set('Authorization', 'Bearer test');
 
     // then
     expect(response.statusCode).toBe(200);
@@ -23,11 +31,12 @@ describe('Fetch todo', () => {
 describe('Fetch todos', () => {
   test('It should return unauthorized when no token was provided', async () => {
     // given
-    const expectedResponseBody = { status: 401, code: '0001', message: 'Could not verify JWT token' };
+    const expectedResponseBody = { status: 401, code: '0010', message: 'Authorization header required' };
     todoService.getTodo.mockResolvedValue(expectedResponseBody);
 
     // when
-    const response = await request(app).get('/api/v1/todos');
+    const response = await request(app)
+      .get('/v1/todos');
 
     // then
     expect(response.statusCode).toBe(401);
